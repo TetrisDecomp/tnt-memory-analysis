82d9d4a995a5b6597b0a80d08df20eb35c7ca804
aidanhenry <aidanhenry@gmail.com>
2023-05-08 01:10:28 -0500

    Add stats window to board vis

--- visualize_board.py
+++ visualize_board.py
@@ -7,29 +7,49 @@ pixel_size = 30
 text_height = 25
 dimensions = (board_width*pixel_size, board_height*pixel_size)
 
+stats_width = 256
+stats_height = 256
+
 game_board = [[[] for x in range(board_width)] for y in range(board_height)]
 
 data_path = "./bin/dump.data"
 piece_colors = ["pink", "purple", "red", "green", "yellow", "cyan", "gray", "black"]
-window = tk.Tk()
-canvas = tk.Canvas(window, width=dimensions[0], height=dimensions[1]+text_height)
-coordinates = canvas.create_text(dimensions[0]/2, dimensions[1]+(text_height/2), text='', fill="black", font='24')
+board_window = tk.Tk()
+board_window.title("board")
+canvas = tk.Canvas(board_window, width=dimensions[0], height=dimensions[1]+text_height)
+
+stats_window = tk.Tk()
+stats_window.title("stats")
+stats_canvas = tk.Canvas(stats_window, width=stats_width, height=stats_height)
+coordinates = stats_canvas.create_text(stats_width/2, stats_height/2, text='', fill="black", font='24')
 
 def get_mouse_position(e):
     x= e.x//pixel_size
     y= e.y//pixel_size
     if 0<=x<board_width and 0<=y<board_height:
-        canvas.itemconfigure(coordinates, text=f'{game_board[y][x].hex()}')
+        cell = game_board[y][x]
+        stats_canvas.itemconfigure(coordinates, text=
+                                   f'00: {cell[0]}' +
+                                   f'\npiece type: {piece_colors[cell[1]]}' +
+                                   f'\n02: {cell[2]}' +
+                                   f'\nsquare number: {cell[3]}' +
+                                   f'\nconnected cell: {cell[4:7].hex()}' +
+                                   f'\nmemory id: {cell[8]}' +
+                                   f'\ncoordinates: {cell[9]}, {cell[10]}' +
+                                   f'\n11: {cell[11]}' +
+                                   f'\ngraphics ref: {cell[12:15].hex()}')
     else:
-        canvas.itemconfigure(coordinates, text='')
+        stats_canvas.itemconfigure(coordinates, text='')
 
+stats_canvas.pack(expand="yes", fill="both")
 
 canvas.bind('<Motion>', get_mouse_position)
 
 canvas.pack(expand="yes", fill="both")
 canvas.configure(bg="white")
 
-window.geometry(f'{dimensions[0]}x{dimensions[1]+text_height}')
+
+board_window.geometry(f'{dimensions[0]}x{dimensions[1]}')
 
 for index in range(board_width*board_height):
     x_coord = index % 10
@@ -38,8 +58,8 @@ for index in range(board_width*board_height):
     canvas.create_rectangle(x_coord*pixel_size, y_coord*pixel_size, x_coord*pixel_size+pixel_size, y_coord*pixel_size+pixel_size, fill=fill, outline="white")
 
 while True:
-    window.update()
-    window.update_idletasks()
+    board_window.update()
+    board_window.update_idletasks()
     with open(data_path, mode="rb") as file:
         try:
             data = file.read()
