12d98f62b558596eb08d0f7ff686cc54776408b9
aidanhenry <aidanhenry@gmail.com>
2023-05-25 22:50:23 -0500

    Render x sub-coordinate

--- visualize_board.py
+++ visualize_board.py
@@ -22,6 +22,16 @@ STATS_WIDTH = 256
 STATS_HEIGHT = 256
 TEXT_HEIGHT = 25
 
+piece_colors = [{"name": 'L', "color": '#c148cf'},
+                {"name": 'J', "color": '#602b8f'}, 
+                {"name": 'Z', "color": '#c41f1f'}, 
+                {"name": 'S', "color": '#21bf3b'}, 
+                {"name": 'T', "color": '#d7de1b'}, 
+                {"name": 'I', "color": '#3fc1e8'}, 
+                {"name": 'O', "color": '#b2b6b8'}, 
+                {"name": 'empty', 'color': '#000000'}] 
+
+
 def swap32(x):                                         
     return int.from_bytes(x, byteorder='little', signed=False).to_bytes(4, byteorder='big')
 
@@ -36,12 +46,12 @@ def parse_int_bitfield(amount, integer):
         amount -= 1
     return result
 
-def process_offset(address):
+def process_offset(address): # Project64 memory offset
      return address - 0x80000000 + 0xDFE40000
 
 def deref(address):
     buffer = ctypes.create_string_buffer(4) 
-    rPM(process.handle,process_offset(address),buffer,4,ctypes.byref(bytes_read))
+    read_process_memory(process.handle,process_offset(address),buffer,4,ctypes.byref(bytes_read))
     cell_bytes = bytes(buffer)
     chunked = more_itertools.chunked(cell_bytes, 4)
     chunked_list = list(chunked)
@@ -51,29 +61,20 @@ def deref(address):
 win_mi = wmi.WMI()
 processes = [process for process in win_mi.Win32_Process()]
 pid = int(list(filter(lambda process : "Project64" in process.name, processes))[0].Handle)
-
 process = win32api.OpenProcess(PROCESS_ALL_ACCESS,0,pid)
 
-rPM = ctypes.WinDLL('kernel32',use_last_error=True).ReadProcessMemory
-rPM.argtypes = [wintypes.HANDLE,ctypes.wintypes.LPCVOID,wintypes.LPVOID,ctypes.c_size_t,ctypes.POINTER(ctypes.c_size_t)]
-rPM.restype = wintypes.BOOL
+read_process_memory = ctypes.WinDLL('kernel32',use_last_error=True).ReadProcessMemory
+read_process_memory.argtypes = [wintypes.HANDLE,ctypes.wintypes.LPCVOID,wintypes.LPVOID,ctypes.c_size_t,ctypes.POINTER(ctypes.c_size_t)]
+read_process_memory.restype = wintypes.BOOL
 bytes_read = ctypes.c_size_t()
 
-mobile_piece_address = deref(MOBILE_PIECE_POINTER)
-offset_mobile_piece_address = process_offset(mobile_piece_address)
-mobile_piece_buffer = ctypes.create_string_buffer(MOBILE_PIECE_BUFFER_SIZE) 
+def read_memory(address, size): # Project64-specific read function
+    offset_address = process_offset(address)
+    buffer = ctypes.create_string_buffer(size)
+    read_process_memory(process.handle,offset_address,buffer,size,ctypes.byref(bytes_read))
+    return swap_bytearray(bytes(buffer))
 
-board_address = deref(deref(BOARD_POINTERS))
-offset_board_address = process_offset(board_address)
-board_buffer = ctypes.create_string_buffer(BOARD_BUFFER_SIZE)
-
-trig_address = TRIG_ADDRESS
-offset_trig_address = process_offset(trig_address)
-trig_buffer = ctypes.create_string_buffer(TRIG_BUFFER_SIZE)
-
-rPM(process.handle,offset_trig_address,trig_buffer,TRIG_BUFFER_SIZE,ctypes.byref(bytes_read))
-
-trig_data = struct.unpack('<8i', trig_buffer)
+trig_data = struct.unpack('>8i', read_memory(TRIG_ADDRESS, TRIG_BUFFER_SIZE))
 
 game_board = [[[] for x in range(BOARD_WIDTH)] for y in range(BOARD_HEIGHT)]
 mobile_piece = b''
@@ -84,15 +85,6 @@ y_offset = 0
 mobile_piece_type = 7
 mobile_piece_minos = [0, 0, 0, 0]
 mobile_outline_minos = [0, 0, 0, 0]
-
-piece_colors = [{"name": 'L', "color": '#c148cf'},
-                {"name": 'J', "color": '#602b8f'}, 
-                {"name": 'Z', "color": '#c41f1f'}, 
-                {"name": 'S', "color": '#21bf3b'}, 
-                {"name": 'T', "color": '#d7de1b'}, 
-                {"name": 'I', "color": '#3fc1e8'}, 
-                {"name": 'O', "color": '#b2b6b8'}, 
-                {"name": 'empty', 'color': '#000000'}] 
 
 board_window = tk.Tk()
 board_window.title("board")
@@ -133,24 +125,15 @@ canvas.configure(bg="white")
 
 board_window.geometry(f'{dimensions[0]}x{dimensions[1]}')
 
-for index in range(BOARD_WIDTH*BOARD_HEIGHT):
-    x_coord = index % 10
-    y_coord = index // 10
-    canvas.create_rectangle(x_coord*PIXEL_SIZE, y_coord*PIXEL_SIZE, x_coord*PIXEL_SIZE+PIXEL_SIZE, y_coord*PIXEL_SIZE+PIXEL_SIZE, fill="black")
-
 while True:
     board_window.update()
     board_window.update_idletasks()
     board_address = deref(deref(BOARD_POINTERS))
-    offset_board_address = process_offset(board_address)
-    rPM(process.handle,offset_board_address,board_buffer,BOARD_BUFFER_SIZE,ctypes.byref(bytes_read))
     mobile_piece_address = deref(MOBILE_PIECE_POINTER)
-    offset_mobile_piece_address = process_offset(mobile_piece_address)
-    rPM(process.handle,offset_mobile_piece_address,mobile_piece_buffer,MOBILE_PIECE_BUFFER_SIZE,ctypes.byref(bytes_read))
+    mobile_piece = read_memory(mobile_piece_address, MOBILE_PIECE_BUFFER_SIZE)
+    board_data = read_memory(board_address,BOARD_BUFFER_SIZE)
 
     try:
-        mobile_piece = swap_bytearray(bytes(mobile_piece_buffer))
-        board_data = swap_bytearray(bytes(board_buffer))
         if len(board_data) > 0:
             for index in range(BOARD_WIDTH*BOARD_HEIGHT):
                 start_address = index*16
@@ -160,53 +143,54 @@ while True:
 
                 if game_board[y_coord][x_coord] != cell:
                     fill = piece_colors[cell[1]]["color"]
-                    outline = '#404040'
+                    outline = '#303030'
                     canvas.create_rectangle(x_coord*PIXEL_SIZE, y_coord*PIXEL_SIZE, x_coord*PIXEL_SIZE+PIXEL_SIZE, y_coord*PIXEL_SIZE+PIXEL_SIZE, fill=fill, outline=outline)
                     game_board[y_coord][x_coord] = cell
 
         if len(mobile_piece) > 0:
-            orientation_data_address = int.from_bytes(mobile_piece[36:40])
-            offset_orientation_data_address = process_offset(orientation_data_address)
-            orientation_data_buffer = ctypes.create_string_buffer(ORIENTATION_DATA_BUFFER_SIZE) 
-            rPM(process.handle,offset_orientation_data_address,orientation_data_buffer,ORIENTATION_DATA_BUFFER_SIZE,ctypes.byref(bytes_read))
-            orientation_data = swap_bytearray(bytes(orientation_data_buffer))
-
-            
+            is_mobile = mobile_piece[0]
             mobile_x_coord = mobile_piece[17]
             mobile_y_coord = mobile_piece[18]
+            rendered_mobile_x_coord = mobile_piece[46]
+            mobile_x_subcoord = mobile_piece[47]
             rendered_mobile_y_coord = mobile_piece[48]
             mobile_y_subcoord = mobile_piece[49]
             rotation_state = mobile_piece[10] 
             sine = trig_data[rotation_state*2]
             cosine = trig_data[rotation_state*2+1]
+
             if mobile_piece_minos[0] != 0:
                 for mino in mobile_piece_minos:
                     canvas.delete(mino)
                 for mino in mobile_outline_minos:
                     canvas.delete(mino)
             mobile_piece_type = mobile_piece[19]
-            for index in range(4):
-                x_offset = orientation_data[index*2+1] - mobile_piece[15]
-                y_offset = orientation_data[index*2+2] - mobile_piece[16]
-                new_x_coord = (x_offset*cosine-y_offset*sine)+mobile_x_coord
-                new_y_coord = (x_offset*sine+y_offset*cosine)+rendered_mobile_y_coord
-                fill = piece_colors[mobile_piece[19]]["color"]
-                outline = '#404040'
-                mobile_piece_minos[index] = canvas.create_rectangle((new_x_coord)*PIXEL_SIZE,
-                                        (new_y_coord)*PIXEL_SIZE + (mobile_y_subcoord / 255) // (1/PIXEL_SIZE),
-                                        (new_x_coord)*PIXEL_SIZE+PIXEL_SIZE,
-                                        (new_y_coord)*PIXEL_SIZE+PIXEL_SIZE + (mobile_y_subcoord / 255) // (1/PIXEL_SIZE), 
-                                        fill=fill,
-                                        outline=outline)
-                new_y_coord = (x_offset*sine+y_offset*cosine)+mobile_y_coord
-                fill = ''
-                outline = 'orange'
-                mobile_outline_minos[index] = canvas.create_rectangle((new_x_coord)*PIXEL_SIZE,
-                                        (new_y_coord)*PIXEL_SIZE,
-                                        (new_x_coord)*PIXEL_SIZE+PIXEL_SIZE,
-                                        (new_y_coord)*PIXEL_SIZE+PIXEL_SIZE,
-                                        fill=fill,
-                                        outline=outline)
+            if is_mobile == 1:
+                orientation_data_address = int.from_bytes(mobile_piece[36:40])
+                orientation_data = read_memory(orientation_data_address, ORIENTATION_DATA_BUFFER_SIZE)
+                for index in range(4):
+                    x_offset = orientation_data[index*2+1] - mobile_piece[15]
+                    y_offset = orientation_data[index*2+2] - mobile_piece[16]
+                    new_x_coord = (x_offset*cosine-y_offset*sine)+rendered_mobile_x_coord
+                    new_y_coord = (x_offset*sine+y_offset*cosine)+rendered_mobile_y_coord
+                    fill = piece_colors[mobile_piece[19]]["color"]
+                    outline = '#303030'
+                    mobile_piece_minos[index] = canvas.create_rectangle((new_x_coord)*PIXEL_SIZE + (mobile_x_subcoord / 255) // (1/PIXEL_SIZE),
+                                            (new_y_coord)*PIXEL_SIZE + (mobile_y_subcoord / 255) // (1/PIXEL_SIZE),
+                                            (new_x_coord)*PIXEL_SIZE+PIXEL_SIZE + (mobile_x_subcoord / 255) // (1/PIXEL_SIZE),
+                                            (new_y_coord)*PIXEL_SIZE+PIXEL_SIZE + (mobile_y_subcoord / 255) // (1/PIXEL_SIZE), 
+                                            fill=fill,
+                                            outline=outline)
+                    new_x_coord = (x_offset*cosine-y_offset*sine)+mobile_x_coord
+                    new_y_coord = (x_offset*sine+y_offset*cosine)+mobile_y_coord
+                    fill = ''
+                    outline = '#996312'
+                    mobile_outline_minos[index] = canvas.create_rectangle((new_x_coord)*PIXEL_SIZE,
+                                            (new_y_coord)*PIXEL_SIZE,
+                                            (new_x_coord)*PIXEL_SIZE+PIXEL_SIZE,
+                                            (new_y_coord)*PIXEL_SIZE+PIXEL_SIZE,
+                                            fill=fill,
+                                            outline=outline)
 
     except Exception as error:
         print(error)
